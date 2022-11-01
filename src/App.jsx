c6c292914944e53e74eb8b075682330ba5e5a5ce
aylindoganer <doganeraylinn@gmail.com>
2022-11-01 17:43:49 +0200

    rendered search movie component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-
 import './App.css'
+import SearchMovie from '../Components/SearchMovie'
+
 
 function App() {
-  
+    return(
+        <div className='container'>
+            <h1 className='title'>Movie Search</h1>
+            <SearchMovie />
+        </div>
+    )
+
 }
 
 export default App

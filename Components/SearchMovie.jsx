4334505e22121119ce4510a11349652b6ee7c295
aylindoganer <doganeraylinn@gmail.com>
2022-11-02 12:06:54 +0200

    imported card component

--- Components/SearchMovie.jsx
+++ Components/SearchMovie.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-
+import Card from "./Card"
 export default function SearchMovie() {
 
     const [query, setQuery] = useState("")
@@ -38,18 +38,14 @@ export default function SearchMovie() {
         </form>
         <div className="card-list">
             {movies.filter(movie => movie.poster_path).map(movie => (
-                <div className="card" key={movie.id}>
-                    <img className="card--image"
-                        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                        alt={movie.title + ' poster'}
-                    />
-                    <div className="card--content">
-                        <h3 className="card--title">{movie.title}</h3>
-                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                        <p><small>RATING: {movie.vote_average}</small></p>
-                        <p className="card--desc">{movie.overview}</p>
-                    </div>
-                </div>
+                <Card key={movie.id}
+                      poster={`${movie.poster_path}`}
+                      altText={movie.title}
+                      title={movie.title}
+                      releaseDate={movie.release_data}
+                      rating={movie.vote_average}
+                      overview={movie.overview}       
+                />
             ))}
         </div>    
         </>
